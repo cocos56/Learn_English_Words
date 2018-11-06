a3b9a971ebc0d290091e5c6b3aefc2168bf5cb08
COCO5666 <zj175@139.com>
2018-11-06 12:50:21 +0800

    COCO5666

--- 自动提交 - 副本.bat
+++
@@ -1,7 +0,0 @@
-git remote add origin git@github.com:Learn_English_Words.git
-git status
-git add --all
-git commit -m "COCO5666"
-git push -u origin master
-start https://github.com/COCO5666?tab=repositories
-set /p a=输入任意键退出程序:
