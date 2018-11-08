9807b3a2533c84be78ee30e6772b7e0eee927107
COCO5666 <zj175@139.com>
2018-11-08 13:43:21 +0800

    COCO5666

--- 自动提交.bat
+++ 自动提交.bat
@@ -1,40 +1,9 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCuDJAmn22EfFJ2ogINFzVc/wnKYMgR/NwACb4ubqB0D8GQKUntDpDj61s0DvxPHHvy1aE/WyFVdZhfL
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+d:
+cd "D:\0COCO\本科\大二上学期\英语三"
 git remote add origin git@github.com:Learn_English_Words.git
 git status
 git add --all
 git commit -m "COCO5666"
 git push -u origin master
 start https://github.com/COCO5666?tab=repositories
-set /p a=杈撳叆浠绘剰閿��鍑虹▼搴?
+set /p a=Press any key to quit .:
